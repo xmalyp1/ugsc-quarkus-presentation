e8f9ecf1fed84b9368fac36c87c46bdf967180a6
Patrik Maly <maly.patrik@uniqa.at>
2022-05-05 13:39:08 +0200

    Updated README.md

--- src/test/java/ugsc/presentations/DatabaseConnectionTest.java
+++ src/test/java/ugsc/presentations/DatabaseConnectionTest.java
@@ -1,15 +1,16 @@
 package ugsc.presentations;
 
+import io.quarkus.logging.Log;
 import io.quarkus.test.TestTransaction;
 import io.quarkus.test.junit.QuarkusTest;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import ugsc.presentations.model.Greeting;
 import ugsc.presentations.service.GreetingService;
 
 import javax.inject.Inject;
 
 @QuarkusTest
+@TestMethodOrder(value = MethodOrderer.OrderAnnotation.class)
 public class DatabaseConnectionTest {
 
     @Inject
@@ -17,15 +18,19 @@ public class DatabaseConnectionTest {
 
     @TestTransaction
     @Test
-    public void testPersistGreeting_failOtherwise(){
+    @Order(value = 1)
+    public void testCreateGreeting_failOtherwise(){
         Greeting greeting = new Greeting("Test Greeting rolled back","Patrik M");
         greeting.persist();
         Assertions.assertTrue(greeting.isPersistent());
+        Log.info("Persisted entity in test, but rollback is performed");
     }
 
     @Test
+    @Order(value = 2)
     public void testFetchGreeting_failOtherwise(){
         Assertions.assertTrue(greetingService.getGreetings().isEmpty());
+        Log.info("Empty DB.");
     }
 
 }
